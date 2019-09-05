7ab86f42d8c32a58673ca62010d814c01adf9040
StarFoxy <ctc5000@yandex.ru>
2019-09-05 12:27:46 +0400

    Relise

---
+++ Assets/Scripts/StaticScripts/ArenaCreator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaCreator : MonoBehaviour
+{
+    public static GameObject ArenaLoadGenerator(GameObject ArenaPrefab, int Width, int Height)
+    {
+        GameObject _Arena = Instantiate<GameObject>(ArenaPrefab);
+        _Arena.GetComponent<SampleSprite>().size = new Vector2(Width,Height);
+        return _Arena;
+    }
+  
+}
---
+++ Assets/Scripts/StaticScripts/CloseProgram.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseProgram : MonoBehaviour
+{
+
+    public void Close()
+    {
+        Application.Quit();
+    }
+}
